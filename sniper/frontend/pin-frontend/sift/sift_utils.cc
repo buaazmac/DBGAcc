fb0a092a76cbe8492cb9561ad0ffd1b8f32e99d7
zmac <zmxmac@hotmail.com>
2020-06-29 23:06:19 -0700

    sniper folder

---
+++ sniper/frontend/pin-frontend/sift/sift_utils.cc
@@ -0,0 +1,1 @@
+/media/zmac/SpinDisk/Research/PIM/full_sys_simulator/test/sniper_pim_prof/sniper/frontend/pin-frontend/../../sift/sift_utils.cc
---
+++ sniper/sift/recorder/sift/zfstream.h
@@ -0,0 +1,1 @@
+/media/zmac/SpinDisk/Research/PIM/full_sys_simulator/test/sniper_pim_prof/sniper/sift/recorder/../../sift/zfstream.h
